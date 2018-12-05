58849264131efcf060a5086300168a17f8777263
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-12-05 23:00:25 +0000

    implemented category display for admin view

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: %i[edit update show]
+    resources :categories, only: %i[index new create destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
