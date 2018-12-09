c64bb6e2dfb2f43e23471ae9d8362ab373256aa9
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-12-09 23:23:50 +0000

    basic auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    @user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if @user&.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user
-      # logged in when they navigate around our website.
-      session[:user_id] = @user.id
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      user_session = User.find_by_email(params[:email])
+      session[:user_id] = user_session.id
       redirect_to root_url, notice: 'Logged in!'
     else
       # If user's login doesn't work, send them back to the login form.
